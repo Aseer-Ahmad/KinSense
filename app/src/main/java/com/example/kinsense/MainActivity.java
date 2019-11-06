b6f594a865f1b9e2c6e0f01f5279d36b65d25bd4
Aseer-Ahmad <a3ahmad.kiit@gmail.com>
2019-11-06 12:57:21 +0530

    Scan

--- app/src/main/java/com/example/kinsense/MainActivity.java
+++ app/src/main/java/com/example/kinsense/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                enableBluetooth();
+                //enableBluetooth();
 
             }
         });
