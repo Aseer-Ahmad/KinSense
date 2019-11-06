6e4bd2c76a22d44e2b184777e44bbd01d0e8d2cb
Aseer-Ahmad <a3ahmad.kiit@gmail.com>
2019-11-06 12:28:48 +0530

    initial

---
+++ app/src/main/java/com/example/kinsense/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.kinsense;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    //components
+    private Button b1;
+    private TextView t1;
+
+    //resources
+    private BluetoothAdapter bluetoothAdapter = null;
+
+    //CONSTANTS
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        t1 = findViewById(R.id.textview_paired_devices);
+        b1 = findViewById((R.id.button_bluetooth_enable));
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                enableBluetooth();
+
+            }
+        });
+
+
+    }
+
+    private void enableBluetooth() {
+
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+
+        }else if(!bluetoothAdapter.isEnabled()){
+
+            //directly enable without asking for permission
+            //bluetoothAdapter.enable();
+
+            //pop up a dialog requesting to turn on the bluetooth
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }else if (bluetoothAdapter.isEnabled()){
+
+            showPairedDevices();
+            Toast.makeText(this, "Bluetooth is already enabled!! " , Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void showPairedDevices() {
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        String alldevice = "";
+        int totaldevices = pairedDevices.size();
+
+        if (totaldevices > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+
+                // for displaying information
+                alldevice +=  deviceName + " : " + deviceHardwareAddress + "\n";
+            }
+        }
+        t1.setText(alldevice);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == REQUEST_ENABLE_BT){
+            if(resultCode != Activity.RESULT_OK){
+
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }else{
+                // bluetooth has been allowed by the user in the dialog
+                showPairedDevices();
+            }
+        }
+
+    }
+
+}
