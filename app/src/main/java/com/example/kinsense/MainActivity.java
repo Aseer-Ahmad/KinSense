40972358e703e920e92c8e12f98b6530d3c7265b
Aseer-Ahmad <a3ahmad.kiit@gmail.com>
2019-11-06 15:15:27 +0530

    added layout

--- app/src/main/java/com/example/kinsense/MainActivity.java
+++ app/src/main/java/com/example/kinsense/MainActivity.java
@@ -42,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 //enableBluetooth();
-
+                Intent intent = new Intent(MainActivity.this, Scan.class);
+                startActivity(intent);
             }
         });
 
---
+++ app/src/main/java/com/example/kinsense/Scan.java
@@ -0,0 +1,20 @@
+package com.example.kinsense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class Scan extends AppCompatActivity {
+
+    private ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+
+
+
+    }
+}
