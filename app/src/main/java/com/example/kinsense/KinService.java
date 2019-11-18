208fb57a5a4294d79c28763d017bb786bd2503d6
Aseer-Ahmad <a3ahmad.kiit@gmail.com>
2019-11-18 17:00:27 +0530

    check CallAPI HostUnknown EXCEPTIONS & KinService updated

--- app/src/main/java/com/example/kinsense/KinService.java
+++ app/src/main/java/com/example/kinsense/KinService.java
@@ -1,6 +1,9 @@
 package com.example.kinsense;
 
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothManager;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
@@ -10,6 +13,17 @@ import androidx.annotation.Nullable;
 
 public class KinService extends Service{
 
+    private final static String TAG = KinService.class.getSimpleName();
+
+    private BluetoothManager mBluetoothManager;
+    private BluetoothAdapter mBluetoothAdapter;
+    private String mBluetoothDeviceAddress;
+    private BluetoothGatt mBluetoothGatt;
+    private int mConnectionState = STATE_DISCONNECTED;
+
+    private static final int STATE_DISCONNECTED = 0;
+    private static final int STATE_CONNECTING = 1;
+    private static final int STATE_CONNECTED = 2;
 
     public class LocalBinder extends Binder {
         KinService getService() {
